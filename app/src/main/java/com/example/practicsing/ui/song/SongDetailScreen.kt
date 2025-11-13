6ea3677bab6c41157f4e6df3c9411c0f9470dd42
yousrchive <oujungprof@gmail.com>
2025-11-13 12:57:23 +0900

    Feat: add TextField component to be used in both login and signup

--- app/src/main/java/com/example/practicsing/ui/song/SongDetailScreen.kt
+++ app/src/main/java/com/example/practicsing/ui/song/SongDetailScreen.kt
@@ -4,7 +4,7 @@ package com.example.practicsing.ui.song
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import com.example.practicsing.ui.song.common.SongDetailContent
+import com.example.practicsing.ui.common.SongDetailContent
 
 @Composable
 fun SongDetailScreen(
